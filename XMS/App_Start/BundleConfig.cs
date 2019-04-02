63621188641562ae04beebd78e3e02fbc8495f6a
dupeng0224 <4909004@qq.com>
2019-04-02 23:34:57 +0800

    增加控制器ProductSku

--- XMS/App_Start/BundleConfig.cs
+++ XMS/App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace XMS
         // 有关绑定的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            #region 框架页面
             bundles.Add(new ScriptBundle("~/js/index").Include(
                 "~/Scripts/app.min.js",
                 "~/Scripts/bootstrap.min.js",
@@ -19,7 +20,27 @@ namespace XMS
                 "~/Content/AdminLTE.min.css",
                 "~/Content/bootstrap.min.css",
                 "~/Content/fonts/font-awesome.min.css",
-                "~/Content/skin-blue.min.css"));       
+                "~/Content/skin-blue.min.css"));
+            #endregion
+
+            #region 框架内页
+            bundles.Add(new ScriptBundle("~/js/page").Include(
+                "~/Scripts/jquery-3.3.1.min.js",
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/plugins/bootstrap-editable.js",
+                "~/Scripts/plugins/bootstrap-table/bootstrap-table.min.js",
+                "~/Scripts/plugins/bootstrap-table/bootstrap-table-editable.js",
+                "~/Scripts/plugins/bootstrap-table/bootstrap-table-zh-CN.min.js",                
+                "~/Scripts/plugins/bootstrapValidator/bootstrapValidator.min.js"));
+
+            bundles.Add(new ScriptBundle("~/css/page").Include(
+                "~/Content/bootstrap.min.css",
+                "~/Content/animate.min.css",                
+                "~/Content/fonts/font-awesome.min.css",
+                "~/Content/plugins/bootstrap-table/bootstrap-table.min.css",
+                "~/Content/plugins/bootstrapValidator/bootstrapValidator.min.css",
+                "~/Content/plugins/bootstrap-editable.css"));
+            #endregion
         }
     }
 }
---
+++ XMS/Controllers/ProductSkuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XMS.Controllers
+{
+    public class ProductSkuController : Controller
+    {
+        // GET: ProductSku
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
