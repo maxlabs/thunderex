0fb4230d78380a55ab5596cdb875e8d4da4a016e
478632418 <478632418@qq.com>
2019-04-11 21:53:21 +0800

    添加订单管理

---
+++ XMS/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XMS.Models;
+
+namespace XMS.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        [HttpGet]
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Load() {
+            List<OrderModel> orders = new List<OrderModel>();
+            orders.Add(new OrderModel(1, "Order 1 Test ", "XHD-I23LKDSL0-8234", false));
+            orders.Add(new OrderModel(2, "Order 1 Test ", "XHD-I23LKDSL0-8234", false));
+            orders.Add(new OrderModel(3, "Order 1 Test ", "XHD-I23LKDSL0-8234", false));
+            orders.Add(new OrderModel(4, "Order 1 Test ", "XHD-I23LKDSL0-8234", false));
+            orders.Add(new OrderModel(5, "Order 1 Test ", "XHD-I23LKDSL0-8234", false));
+            orders.Add(new OrderModel(6, "Order 1 Test ", "XHD-I23LKDSL0-8234", false));
+            orders.Add(new OrderModel(7, "Order 1 Test ", "XHD-I23LKDSL0-8234", false));
+            orders.Add(new OrderModel(8, "Order 1 Test ", "XHD-I23LKDSL0-8234", false));
+            orders.Add(new OrderModel(9, "Order 1 Test ", "XHD-I23LKDSL0-8234", false));
+            orders.Add(new OrderModel(10, "Order 1 Test ", "XHD-I23LKDSL0-8234", false));
+
+            string strJson = JsonConvert.SerializeObject(orders);
+            JsonResult result= new JsonResult();
+            result.Data = strJson;
+
+            return result;
+        }
+    }
+}
---
+++ XMS/Models/OrderModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace XMS.Models
+{
+    public class OrderModel
+    {
+        private long id;
+        private string title;
+        private string paId;
+        private bool isAgent;
+
+        public OrderModel() { }
+        public OrderModel(long id, string title, string paId, bool isAgent)
+        {
+            this.id = id;
+            this.title = title;
+            this.paId = paId;
+            this.isAgent = isAgent;                
+        }
+
+        public long Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                title = value;
+            }
+        }
+
+        public string PaId
+        {
+            get
+            {
+                return paId;
+            }
+
+            set
+            {
+                paId = value;
+            }
+        }
+
+        public bool IsAgent
+        {
+            get
+            {
+                return isAgent;
+            }
+
+            set
+            {
+                isAgent = value;
+            }
+        }
+    }
+}
